6e267ab27cfb40943333e1a754dad491b66bbcf9
alex <alexx.charalampidi@gmail.com>
2021-10-22 09:04:40 +0300

    new flock

---
+++ flockStillEdges211022/src/boid.h
@@ -0,0 +1,31 @@
+#pragma once
+
+// #include "ofMain.h"
+// #include "ofApp.h"
+
+class boid{
+
+public:
+    void setup(float width,  float height);
+    ofVec2f align(std::vector<boid> flock);
+    ofVec2f cohesion(std::vector<boid> flock);
+    ofVec2f separation(std::vector<boid> flock);
+    ofVec2f repulsion(std::vector<boid> flock, std::vector<boid> flock1);
+    void flock(std::vector<boid> flock);
+    void update();
+    void draw(int boolean, ofColor color, std::vector<boid> flock);
+    void edges();
+
+    ofVec2f position;
+    ofVec2f velocity;
+    ofVec2f acceleration;
+    ofVec2f direction;
+    float maxForce;
+    float maxSpeed;
+    float semult, comult, almult, velx, vely;
+    int alignRadius;
+    int size;
+    deque<glm::vec2> log;
+
+};
+extern boid bo;
